12413068ddde839dffd8f37e8616599437723595
Kimia Kalantari <kimiakalantari1356@gmail.com>
2018-01-14 17:37:56 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,6 +1,6 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
- var newArray = ["element", ...array];
+ var newArray = [element, ...array];
  return newArray, array
 }
 
