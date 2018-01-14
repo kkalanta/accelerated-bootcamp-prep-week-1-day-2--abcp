8509e06b0dd00326d105362d51f9bade529ff7eb
Kimia Kalantari <kimiakalantari1356@gmail.com>
2018-01-14 18:49:19 +0000

    automatically backed up by learn

--- if.js
+++ if.js
@@ -4,5 +4,10 @@ if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
 
-
-
+function greaterThanTen(num){
+  if (num === 10){
+    return num
+  } else if (num <= 10){
+    return "Sorry, your number is not 10"
+  }
+}
