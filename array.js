ee5670131f71d6869c19d55e76d8eff75c1ee331
Kimia Kalantari <kimiakalantari1356@gmail.com>
2018-01-14 18:14:47 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -35,10 +35,13 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
 
 }
 
-function removeElementFromBeginningOfArray(array) {
+function removeElementFromBeginningOfArray(array, element) {
+  return array.slice(1)
+
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
 
 }
 
