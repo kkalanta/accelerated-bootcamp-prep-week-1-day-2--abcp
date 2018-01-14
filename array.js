e30b158f87c912e8342f0071e73f45e6dbca7c26
Kimia Kalantari <kimiakalantari1356@gmail.com>
2018-01-14 18:13:28 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -29,9 +29,9 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-  newArray = array.shift();
-  return newArray
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+  array.shift(element );
+  return array
 
 }
 
